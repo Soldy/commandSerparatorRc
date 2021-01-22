c74b47aa880948b535955bd79cc2ca571d33e286
Soldy <4786022+Soldy@users.noreply.github.com>
2021-01-22 23:40:26 +0000

    fix
    Signed-off-by: Soldy <4786022+Soldy@users.noreply.github.com>

--- index.js
+++ index.js
@@ -1,51 +1,61 @@
+/*
+ *  @Soldy\poolrc\2021.01.16\GPL3
+ */
+'use strict`;
 
+/*
+ * @param {integer} command
+ * @function
+ * @return {array}
+ */
 const  separatorBase =function (command) {
-        command = command.toString().replace(/(?:(?:^|\n)\s+|\s+(?:$|\n))/g, "").replace(/\s+/g, " ");
-        let commands = [],
-            commandi = 0,
-            commandit = 0,
-            mod = 0,
-            modSelector = "";
-        for (let i = 0; command.length > i; i++) {
-            if (typeof commands[commandi] === "undefined")
-                commands[commandi] = [];
-            if (typeof commands[commandi][commandit] === "undefined")
-                commands[commandi][commandit] = "";
-            if (mod === 0) {
-                if (command.charAt(i) === ";") {
-                    commandi++;
-                    commandit = 0;
-                } else if (command.charAt(i) === "\\") {
-                    mod = 2;
-                } else if (command.charAt(i) === "'") {
-                    mod = 1;
-                    modSelector = "'";
-                } else if (command.charAt(i) === "\"") {
-                    mod = 1;
-                    modSelector = "\"";
-                } else if (
-                    (command.charAt(i) === " ") || (command.charAt(i) === "\t")) {
-                    if (
-                        (i > 0) && (command.charAt(i - 1) !== " ") && 
-                        (command.charAt(i - 1) !== "\t") && 
-                        (command.charAt(i - 1) !== ";") && 
-                        (command.charAt(i - 1) !== "'") && 
-                        (command.charAt(i - 1) !== "\""))
-                        commandit++;
-                } else {
-                    commands[commandi][commandit] += command.charAt(i);
-                }
-            } else if (mod === 1) {
-                if (command.charAt(i) === modSelector) {
-                    mod = 0;
-                } else {
-                    commands[commandi][commandit] += command.charAt(i);
-                }
-            } else if (mod === 2) {
+    command = command.toString().replace(/(?:(?:^|\n)\s+|\s+(?:$|\n))/g, '').replace(/\s+/g, ' ');
+    let commands = [],
+        commandi = 0,
+        commandit = 0,
+        mod = 0,
+        modSelector = '';
+    for (let i = 0; command.length > i; i++) {
+        if (typeof commands[commandi] === 'undefined')
+            commands[commandi] = [];
+        if (typeof commands[commandi][commandit] === 'undefined')
+            commands[commandi][commandit] = '';
+        if (mod === 0) {
+            if (command.charAt(i) === ';') {
+                commandi++;
+                commandit = 0;
+            } else if (command.charAt(i) === '\\') {
+                mod = 2;
+            } else if (command.charAt(i) === '\'') {
+                mod = 1;
+                modSelector = '\'';
+            } else if (command.charAt(i) === '"') {
+                mod = 1;
+                modSelector = '"';
+            } else if (
+                (command.charAt(i) === ' ') || (command.charAt(i) === '\t')) {
+                if (
+                    (i > 0) && (command.charAt(i - 1) !== ' ') && 
+                        (command.charAt(i - 1) !== '\t') && 
+                        (command.charAt(i - 1) !== ';') && 
+                        (command.charAt(i - 1) !== '\'') && 
+                        (command.charAt(i - 1) !== '"'))
+                    commandit++;
+            } else {
                 commands[commandi][commandit] += command.charAt(i);
-                mod = 0;
             }
+        } else if (mod === 1) {
+            if (command.charAt(i) === modSelector) {
+                mod = 0;
+            } else {
+                commands[commandi][commandit] += command.charAt(i);
+            }
+        } else if (mod === 2) {
+            commands[commandi][commandit] += command.charAt(i);
+            mod = 0;
         }
-        return commands;
- };
+    }
+    return commands;
+};
+
 exports.separatorBase = separatorBase;
